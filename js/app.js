1158bb7136e830376b6c5ba23e4c69ef6fe51eea
Alexey Tyulenev <leksey.tyulenev@gmail.com>
2023-03-26 01:24:37 +0300

    переделал js

--- js/app.js
+++ js/app.js
@@ -1,48 +1,60 @@
-$(document).ready(function(){
-    let controls = {
-        video: $("#myvideo"),
-        total: $("#total"),
-        buffered: $("#buffered"),
-        progress: $("#current"),
-        duration: $("#duration"),
-        currentTime: $("#currenttime"),
-        hasHours: false,
+let video = document.getElementById("myvideo");
+let total = document.querySelector("#total");
+let current = document.getElementById("current")
+let duration = document.getElementById("duration");
+let currentTime = document.getElementById("currenttime");
+let pause = document.getElementById('video__btn');
+let back = document.getElementById('back');
+let up = document.getElementById('up')
+let hasHours = false;
+let check = 0;
 
-        togglePlayback: function() {
-            (video.paused) ? video.play($('.video__btn').css('display', 'none')) 
-            : video.pause($('.video__btn').css('display', 'block'));
-        }
-    };
-                
-    let video = controls.video[0];
-
-    video.addEventListener("ended", function() {
+video.addEventListener("click", function() {
+    if (check == 0) {
+        video.play();
+        pause.style.display = 'none';
+        videoPlay = setInterval(function() {
+            let videoTime = Math.round(video.currentTime);
+            let videoLength = Math.round(video.duration);   
+            current.style.width = (videoTime * 100) / videoLength + '%';
+        }, 10)
+        check += 1
+    } else {
         video.pause();
-    });
+        pause.style.display = 'block';
+        clearInterval(videoPlay);
+        check = 0;
+    }
+})
 
-    controls.video.click(function() {
-        controls.togglePlayback();
-    });
-
-    video.addEventListener("canplay", function() {
-        controls.hasHours = (video.duration / 3600) >= 1.0; 
-                           
-        controls.duration.text(formatTime(video.duration, controls.hasHours));
-        controls.currentTime.text(formatTime(0),controls.hasHours);
-    }, false);
-
-    video.addEventListener("timeupdate", function() {
-        controls.currentTime.text(formatTime(video.currentTime, controls.hasHours));
-                        
-        let progress = Math.floor(video.currentTime) / Math.floor(video.duration);
-        controls.progress[0].style.width = Math.floor(progress * controls.total.width()) + "px";
-    }, false);
-
-    controls.total.click(function(e) {
-        let x = (e.pageX - this.offsetLeft)/$(this).width();
-        video.currentTime = x * video.duration;
-    });
+back.addEventListener("click", function() {
+    video.currentTime -= 10;
 });
+
+up.addEventListener("click", function() {
+    video.currentTime += 10;
+});
+
+total.addEventListener("click", function(e) {
+    let posX = e.clientX - 300;
+    let timePos = (posX * 100) / 800;
+    current.style.width = timePos + '%';
+    video.currentTime = (timePos * Math.round(video.duration)) / 100
+});
+
+video.addEventListener("canplay", function() {
+    hasHours = (video.duration / 3600) >= 1.0; 
+                        
+    duration.textContent = formatTime(video.duration, hasHours);
+    currentTime.textContent = formatTime(0), hasHours;
+}, false);
+
+video.addEventListener("timeupdate", function() {
+    currentTime.textContent = formatTime(video.currentTime, hasHours);
+                    
+    let progress = Math.floor(video.currentTime) / Math.floor(video.duration);
+    progress[0].style.width = Math.floor(progress * total.width()) + "px";
+}, false);
 
 function formatTime(time, hours) {
     if (hours) {
