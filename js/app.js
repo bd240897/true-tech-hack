33add42add4f5eb52e775a1bee66475fc3150eb3
Alexey Tyulenev <leksey.tyulenev@gmail.com>
2023-03-26 00:20:52 +0300

    let и адаптив
    минимальный адаптив

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var controls = {
+    let controls = {
         video: $("#myvideo"),
         total: $("#total"),
         buffered: $("#buffered"),
@@ -14,7 +14,7 @@ $(document).ready(function(){
         }
     };
                 
-    var video = controls.video[0];
+    let video = controls.video[0];
 
     video.addEventListener("ended", function() {
         video.pause();
@@ -34,35 +34,35 @@ $(document).ready(function(){
     video.addEventListener("timeupdate", function() {
         controls.currentTime.text(formatTime(video.currentTime, controls.hasHours));
                         
-        var progress = Math.floor(video.currentTime) / Math.floor(video.duration);
+        let progress = Math.floor(video.currentTime) / Math.floor(video.duration);
         controls.progress[0].style.width = Math.floor(progress * controls.total.width()) + "px";
     }, false);
 
     controls.total.click(function(e) {
-        var x = (e.pageX - this.offsetLeft)/$(this).width();
+        let x = (e.pageX - this.offsetLeft)/$(this).width();
         video.currentTime = x * video.duration;
     });
 });
 
 function formatTime(time, hours) {
     if (hours) {
-        var h = Math.floor(time / 3600);
+        let h = Math.floor(time / 3600);
         time = time - h * 3600;
                     
-        var m = Math.floor(time / 60);
-        var s = Math.floor(time % 60);
+        let m = Math.floor(time / 60);
+        let s = Math.floor(time % 60);
                     
         return h.lead0(2)  + ":" + m.lead0(2) + ":" + s.lead0(2);
     } else {
-        var m = Math.floor(time / 60);
-        var s = Math.floor(time % 60);
+        let m = Math.floor(time / 60);
+        let s = Math.floor(time % 60);
                     
         return m.lead0(2) + ":" + s.lead0(2);
     }
 }
             
 Number.prototype.lead0 = function(n) {
-    var nz = "" + this;
+    let nz = "" + this;
     while (nz.length < n) {
         nz = "0" + nz;
     }
